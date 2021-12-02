08e2d7bbef5b6dbf544dd66ecd4aab3879b5b4e8
nadiaf85 <nadia.fiorio@alice.it>
2021-12-02 15:24:29 +0100

    finito

---
+++ js/myscript.js
@@ -0,0 +1,5 @@
+let userInputName = prompt ('Inserisci nome');
+let userInputSurname = prompt ('Inserisci cognome');
+let userPreferColor = prompt ('Inserisci colore preferito');
+
+document.getElementById('psw_gen').innerHTML = userInputName + userInputSurname + userPreferColor + 21;
